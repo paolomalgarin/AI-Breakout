947e545e28fefdb912c596a557cbcd9ec767e187
Pallo <paolomalgarin@gmail.com>
2025-07-10 14:54:25 +0200

    Small fixes

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ import numpy as np
 from assets.scripts.Env import Env
 
 def test_winner(genome_path: str, config_path: str, render: bool = True):
-    # Carica configurazione NEAT
+    # Load NEAT config file
     config = neat.Config(
         neat.DefaultGenome,
         neat.DefaultReproduction,
@@ -15,14 +15,14 @@ def test_winner(genome_path: str, config_path: str, render: bool = True):
         config_path
     )
 
-    # Carica il genoma vincente
+    # Load best genome
     with open(genome_path, 'rb') as f:
         winner = pickle.load(f)
 
-    # Crea la rete feed-forward
+    # Net creation
     net = neat.nn.FeedForwardNetwork.create(winner, config)
 
-    # Inizializza l'ambiente (render=True per pygame display)
+    # Env setup
     env = Env(showTraining=render)
     observation, _ = env.reset()
     done = False
@@ -31,25 +31,23 @@ def test_winner(genome_path: str, config_path: str, render: bool = True):
 
     while not done:
         clock.tick(120)
-        # Prepara lo stato
+        
         state = np.array(observation, dtype=np.float32)
 
-        # Ottieni il vettore di output e scegli l'azione migliore
         action_prob = net.activate(state)
         action = int(np.argmax(action_prob))
 
-        # Esegui l'azione
         observation, reward, terminated, truncated, info = env.step(action)
         done = terminated or truncated
         total_reward += reward
 
     env.close()
 
-    print(f"Reward totale ottenuto dal genoma vincente: {total_reward:.2f}")
+    print(f"Total genome reward --> {total_reward:.2f}")
 
 if __name__ == '__main__':
-    # Percorsi
-    BASE_DIR = os.getcwd()
+    BASE_DIR = os.path.dirname(os.path.realpath(__file__))
+    
     GENOME_PATH = os.path.join(BASE_DIR, 'assets', 'genomes', 'bestGenome.pkl')
     CONFIG_PATH = os.path.join(BASE_DIR, 'assets', 'config', 'neat_config.ini')
 
